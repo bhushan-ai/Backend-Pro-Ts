5e4ee69a0d6767905abe1514c1792173d83d5728
bhushan-ai <bhushaningole2004@gmail.com>
2025-09-24 12:30:02 +0530

    login logout feture

--- Task-manager-Ts/src/controllers/user.controller.ts
+++ Task-manager-Ts/src/controllers/user.controller.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from "express";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+const createToken = (id: string): string => {
+  return jwt.sign({ id }, process.env.SECRET!, {
+    expiresIn: "7d",
+  });
+};
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   const { name, email, password } = req.body;
@@ -36,6 +42,62 @@ export const register = async (req: Request, res: Response): Promise<void> => {
         email: newUser.email,
         role: newUser.role,
       },
+    });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`Something went wrong while registration`, err);
+  }
+};
+
+//login
+export const login = async (req: Request, res: Response): Promise<void> => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      res
+        .status(400)
+        .json({ success: false, message: "Email and Password is required" });
+      return;
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({ success: false, message: "user not found" });
+      return;
+    }
+
+    const matchedPassword = await bcrypt.compare(password, user.password);
+    console.log(matchedPassword);
+    if (!matchedPassword) {
+      res
+        .status(400)
+        .json({ success: false, message: "Password is Incorrect" });
+      return;
+    }
+
+    const token = createToken(user._id.toString());
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    res
+      .status(200)
+      .json({ success: true, message: "User LoggedIn", data: user });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`Something went wrong while registration`, err);
+  }
+};
+export const logout = async (req: Request, res: Response): Promise<void> => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
   } catch (error: unknown) {
     const err = error as Error;
