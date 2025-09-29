c22747145c17bc835b3dc5bb1a27ba8a71b80a4b
bhushan-ai <bhushaningole2004@gmail.com>
2025-09-29 13:03:48 +0530

    notes route added

--- Notes-api-Ts/src/routes/notes.route.ts
+++ Notes-api-Ts/src/routes/notes.route.ts
@@ -1,10 +1,21 @@
 import express from "express";
 import { jwtAuth } from "../services/jwtAuth";
-import { addNote, getNote } from "../controllers/notes.controller";
+import {
+  addNote,
+  deleteNote,
+  getAllNote,
+  getNote,
+  restoreNote,
+  searchNote,
+} from "../controllers/notes.controller";
 
 const noteRouter = express.Router();
 
 noteRouter.post("/add", jwtAuth, addNote);
 noteRouter.get("/get/:id", jwtAuth, getNote);
+noteRouter.get("/getall", jwtAuth, getAllNote);
+noteRouter.get("/search", jwtAuth, searchNote);
+noteRouter.put("/delete/:id", jwtAuth, deleteNote);
+noteRouter.put("/restore/:id", jwtAuth, restoreNote);
 
 export default noteRouter;
