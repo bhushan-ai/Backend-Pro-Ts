8553f836624839e38317bd8e9081256b8a6f00d0
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-01 18:19:22 +0530

    project setup

--- Ecom-Backend-ts/src/server.ts
+++ Ecom-Backend-ts/src/server.ts
@@ -1,13 +1,13 @@
 import express, { Request, Response } from "express";
 
 const PORT = 4000;
-
 const app = express();
 
+//routes
 app.get("/", (req: Request, res: Response) => {
-  res.send("api is working find");
+  res.send("Api is running fine");
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}`);
+  console.log(`Server is started on http://localhost:${PORT}`);
 });
