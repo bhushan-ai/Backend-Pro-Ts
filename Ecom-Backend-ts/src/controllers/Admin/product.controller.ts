bafcfef7030f556715a40aef93c8cd4955bb0581
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-03 18:50:51 +0530

    cloudinary and multer integration image upload route added

--- Ecom-Backend-ts/src/controllers/Admin/product.controller.ts
+++ Ecom-Backend-ts/src/controllers/Admin/product.controller.ts
@@ -1,15 +1,48 @@
 import express, { Request, Response } from "express";
 import User from "../../models/user.model";
 import Product from "../../models/product.model";
+import { imageUploadToCloudinary } from "../../services/cloudinary";
 
+//handle img
+export const handleImage = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    if (!req.file) {
+      res.status(404).json({ success: false, message: "Image not found" });
+      return;
+    }
+    const bufferFile = req.file;
+
+    const b64 = Buffer.from(bufferFile.buffer).toString("base64");
+    const url = `data:${bufferFile.mimetype};base64,${b64}`;
+    const imageUrl = await imageUploadToCloudinary(url);
+
+    if (imageUrl === null) {
+      res
+        .status(404)
+        .json({ success: false, message: "Image url not created" });
+      return;
+    }
+    res
+      .status(201)
+      .json({ success: true, message: "Image uploaded successfully" });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`Something went wrong while uploading image the user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+//add product
 export const addProduct = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-
-    const {name,description,price,salePrice,stock} = req.body 
-
-
+  const { name, description, price, salePrice, stock } = req.body;
 
   try {
     if (!req.user) {
--- Ecom-Backend-ts/src/routes/admin/product.route.ts
+++ Ecom-Backend-ts/src/routes/admin/product.route.ts
@@ -1,7 +1,8 @@
 import express, { Router } from "express";
 
 import { upload } from "../../services/cloudinary";
+import { handleImage } from "../../controllers/Admin/product.controller";
 
 const productRouter = Router();
 
-productRouter.post("/image-upload", upload.single("my_img"));
+productRouter.post("/image-upload", upload.single("my_img"), handleImage);
