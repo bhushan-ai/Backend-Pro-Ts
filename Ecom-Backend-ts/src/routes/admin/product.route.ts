fc0f3e48caf32c6e9bb82cfde08db9a5de7ff17c
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-04 18:25:03 +0530

    product edit route added

--- Ecom-Backend-ts/src/routes/admin/product.route.ts
+++ Ecom-Backend-ts/src/routes/admin/product.route.ts
@@ -1,13 +1,19 @@
 import express, { Router } from "express";
 
 import { upload } from "../../services/cloudinary";
-import { addProduct, handleImage } from "../../controllers/Admin/product.controller";
+import {
+  addProduct,
+  editProduct,
+  handleImage,
+} from "../../controllers/Admin/product.controller";
+import { jwtAuth } from "../../services/jwtAuth";
 
 const productRouter = Router();
 
+//private routes
 productRouter.post("/image-upload", upload.single("my_img"), handleImage);
 
-productRouter.post("/add",  addProduct);
+productRouter.post("/add", jwtAuth, addProduct);
+productRouter.post("/edit/:id", jwtAuth, editProduct);
 
-
-export default productRouter
+export default productRouter;
