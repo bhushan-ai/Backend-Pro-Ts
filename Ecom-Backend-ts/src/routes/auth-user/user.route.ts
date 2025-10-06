a67389d5c6db510189bce0bd1bf92f1a022ab49c
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-06 18:36:09 +0530

    user password update and user details update router added

--- Ecom-Backend-ts/src/routes/auth-user/user.route.ts
+++ Ecom-Backend-ts/src/routes/auth-user/user.route.ts
@@ -1,13 +1,22 @@
 import express, { Router } from "express";
-import { login, logout, register } from "../../controllers/user.controller";
-
-
+import {
+  login,
+  logout,
+  register,
+  updatePassword,
+  updateUser,
+} from "../../controllers/user.controller";
+import { jwtAuth } from "../../services/jwtAuth";
 
 const userRouter = Router();
 
 //public routes
-userRouter.post('/register',register)
-userRouter.post('/login',login)
-userRouter.post('/logout',logout)
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.post("/logout", logout);
 
-export default userRouter
+//private routes
+userRouter.post("/update", jwtAuth, updateUser);
+userRouter.post("/update-password", jwtAuth, updatePassword);
+
+export default userRouter;
