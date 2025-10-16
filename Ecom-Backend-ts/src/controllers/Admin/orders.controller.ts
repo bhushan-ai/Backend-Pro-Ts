8c2835f0445ba6f5c7d5e8228aa67d770e4c6b5b
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-16 18:12:16 +0530

    added the payment

--- Ecom-Backend-ts/src/controllers/Admin/orders.controller.ts
+++ Ecom-Backend-ts/src/controllers/Admin/orders.controller.ts
@@ -1,8 +1,21 @@
-import express, { Request, Response } from "express";
+import { Request, Response } from "express";
 
 export const allOrders = async (req: Request, res: Response): Promise<void> => {
   try {
-    
+      
+  } catch (error) {
+    const err = error as Error;
+    console.log(`Something went wrong while getting the  order's of user`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+//payment
+export const getPayment = async (req: Request, res: Response): Promise<void> => {
+  try {
+      
   } catch (error) {
     const err = error as Error;
     console.log(`Something went wrong while getting the  order's of user`, err);
