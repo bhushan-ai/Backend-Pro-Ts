4433f5c11efb93e2c6a1a2f73fa23536019dc208
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-19 02:20:22 +0530

    resume controller create resume added

---
+++ resume-system-backend-ts/src/controllers/resume.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import Resume from "../model/resume.model";
+
+//create resume
+export const createResume = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { aboutMe, education, skills, projects, experience } = req.body;
+
+    if (!req.user) {
+      res.status(404).json({ success: false, message: "user not found" });
+      return;
+    }
+
+    const id = req.user?._id as string;
+
+    const newResume = await Resume.create({
+      user: id,
+      aboutMe,
+      education,
+      skills,
+      projects,
+      experience,
+    });
+
+    if (!newResume) {
+      res.status(404).json({ success: false, message: "resume not created" });
+      return;
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "resume created successfully",
+      data: newResume,
+    });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`Something went wrong while creating the resume`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
