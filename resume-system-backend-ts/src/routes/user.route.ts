5501b3f7b78738a1f287c046a635c080d6b6b7fe
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-18 17:56:55 +0530

    user route added

---
+++ resume-system-backend-ts/src/routes/user.route.ts
@@ -0,0 +1,22 @@
+import express, { Router } from "express";
+import {
+  login,
+  logout,
+  register,
+  updatePassword,
+  updateUser,
+} from "../controllers/user.controller";
+import { jwtAuth } from "../middlewares/jwtAuth";
+
+const userRouter = Router();
+
+//public routes
+userRouter.post("/register", register);
+userRouter.post("/login", login);
+userRouter.post("/logout", logout);
+
+//private routes
+userRouter.post("/update", jwtAuth, updateUser);
+userRouter.post("/update-password", jwtAuth, updatePassword);
+
+export default userRouter;
