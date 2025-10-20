a75a0da2e58fb6cd97af9a34c11435328e3bf690
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-21 00:51:12 +0530

    fetched controller added

--- resume-system-backend-ts/src/controllers/resume.controller.ts
+++ resume-system-backend-ts/src/controllers/resume.controller.ts
@@ -96,10 +96,59 @@ export const deleteResume = async (
   res: Response
 ): Promise<void> => {
   try {
-    
+    if (!req.user) {
+      res.status(404).json({ success: false, message: "user not found" });
+      return;
+    }
+
+    const id = req.user?._id as string;
+
+    const deletedResume = await Resume.findOneAndDelete({ user: id });
+
+    if (!deletedResume) {
+      res.status(404).json({ success: false, message: "resume not deleted" });
+      return;
+    }
+    res.status(200).json({
+      success: true,
+      message: "resume deleted successfully",
+    });
   } catch (error: unknown) {
     const err = error as Error;
     console.log(`Something went wrong while deleting the resume`, err);
+    res
+      .status(500)
+      .json({ success: false, message: "Server side error", error: err });
+  }
+};
+
+//fetched resume
+export const fetchResume = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { id: resumeId } = req.params;
+
+    if (!resumeId) {
+      res.status(404).json({ success: false, message: "resumeID not found" });
+      return;
+    }
+
+    const resume = await Resume.findById(resumeId);
+    if (!resume) {
+      res.status(404).json({ success: false, message: "resume not found" });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "resume fetched successfully",
+      data: resume,
+    });
+  } catch (error: unknown) {
+    const err = error as Error;
+    console.log(`Something went wrong while fetching the resume`, err);
     res
       .status(500)
       .json({ success: false, message: "Server side error", error: err });
