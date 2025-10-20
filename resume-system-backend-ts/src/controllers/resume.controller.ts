55df9845373c7188a48e79d18d8f76cb6767b23a
bhushan-ai <bhushaningole2004@gmail.com>
2025-10-21 00:41:46 +0530

    delete controller added

--- resume-system-backend-ts/src/controllers/resume.controller.ts
+++ resume-system-backend-ts/src/controllers/resume.controller.ts
@@ -49,28 +49,59 @@ export const updateResume = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-    try {
-        
-    } catch (error: unknown) {
+  try {
+    const { aboutMe, education, skills, projects, experience } = req.body;
+
+    if (!req.user) {
+      res.status(404).json({ success: false, message: "user not found" });
+      return;
+    }
+
+    const id = req.user?._id as string;
+
+    const updatedResume = await Resume.findOneAndUpdate(
+      { user: id },
+      {
+        aboutMe,
+        education,
+        skills,
+        projects,
+        experience,
+      },
+      { new: true }
+    );
+
+    if (!updatedResume) {
+      res.status(404).json({ success: false, message: "resume not update" });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "resume update successfully",
+      data: updatedResume,
+    });
+  } catch (error: unknown) {
     const err = error as Error;
     console.log(`Something went wrong while updating the resume`, err);
     res
       .status(500)
       .json({ success: false, message: "Server side error", error: err });
   }
-}
-//delete resume 
+};
+
+//delete resume
 export const deleteResume = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-    try {
-        
-    } catch (error: unknown) {
+  try {
+    
+  } catch (error: unknown) {
     const err = error as Error;
     console.log(`Something went wrong while deleting the resume`, err);
     res
       .status(500)
       .json({ success: false, message: "Server side error", error: err });
   }
-}
+};
--- resume-system-backend-ts/src/server.ts
+++ resume-system-backend-ts/src/server.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { dbConnection } from "./services/dbconnection";
 const PORT = process.env.PORT || 4000;
 
@@ -8,6 +9,7 @@ const app = express();
 
 //middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
